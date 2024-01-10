81dcfc0043a5a3cc955071301d5a9bf627d8e7f4
Izabela França <ifranca.sc@gmail.com>
2024-01-10 07:30:41 -0300

    Adding readme.

--- object-oriented-programming/practice1/Car.java
+++ object-oriented-programming/practice1/Car.java
@@ -6,7 +6,20 @@ public class Car {
     String model;
     String color;
     int manufactureYear;
+    double buyingPrice;
     Owner owner;
 
+    void calculateResaleValue(int currentYear) {
+
+        int yearsOfUse = currentYear- manufactureYear;
+        var lifeSpan = 20;
+        double resalePrice = (buyingPrice / lifeSpan) * (lifeSpan - yearsOfUse);
+
+        if (resalePrice < 0) {
+            resalePrice = 0;
+        }
+
+        System.out.printf("Resale price: %.2f", resalePrice);
+    }
 
 }
