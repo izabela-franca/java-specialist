6d6732cf0c8fa0dd9cfa32f84999b53f9e4ae61c
Izabela França <ifranca.sc@gmail.com>
2024-01-14 07:01:21 -0300

    Adding readme.

---
+++ object-oriented-programming/practice3/Aircraft.java
@@ -0,0 +1,29 @@
+package practice3;
+
+public class Aircraft {
+
+    boolean active = true;
+    int totalSeats;
+    int bookedSeats;
+
+    int calculateAvailableSeats() {
+        return totalSeats - bookedSeats;
+    }
+
+    void bookSeats(int numberOfSeats) {
+        if (active) {
+            bookedSeats +=numberOfSeats;
+        } else {
+            System.out.printf("The aircraft is inactive. The seats were not booked.\n");
+        }
+    }
+
+    void deactivate() {
+        active = false;
+    }
+
+    void activate() {
+        active = true;
+    }
+
+}
---
+++ object-oriented-programming/practice3/Main.java
@@ -0,0 +1,17 @@
+package practice3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Aircraft plane1 = new Aircraft();
+        plane1.totalSeats = 100;
+        plane1.activate();
+        plane1.bookSeats(10);
+
+        System.out.printf("Plane 1 (%s): %d available seats\n", plane1.active ? "Active" : "Inactive",
+                plane1.calculateAvailableSeats());
+
+    }
+
+}
