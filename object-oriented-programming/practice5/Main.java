64d458900c35fc9af8f6fed70ede5ac0a21a886d
Izabela França <ifranca.sc@gmail.com>
2024-01-27 07:14:53 -0300

    Adding readme.

--- object-oriented-programming/practice5/Main.java
+++ object-oriented-programming/practice5/Main.java
@@ -9,10 +9,15 @@ public class Main {
         newVisitor.age = 12;
 
         if (newVisitor.age < Visitor.MINIMUN_AGE) {
-            System.out.printf("Access not allowed to visitors under %d.", Visitor.MINIMUN_AGE);
+            System.out.printf("Access is not allowed for visitors under %d.\n", Visitor.MINIMUN_AGE);
         } else {
             System.out.println("Access granted.");
         }
 
+        //Local variable type inference (var)
+        var registration = new Registration();
+        registration.register(newVisitor, 2);
+
     }
 }
+
---
+++ object-oriented-programming/practice5/Registration.java
@@ -0,0 +1,21 @@
+package practice5;
+
+public class Registration {
+
+    static final int EXPIRATION_TIME_IN_MONTHS = 1;
+
+    //Overloading
+    void register(Visitor visitor) {
+        this.register(visitor, EXPIRATION_TIME_IN_MONTHS);
+
+    }
+
+    void register(Visitor visitor, int expirationTime) {
+        final int expirationTimeInDays;
+        expirationTimeInDays = expirationTime * 30;
+
+        System.out.printf("Visitor %s is registered for %d days.\n", visitor.name, expirationTimeInDays);
+    }
+
+}
+
