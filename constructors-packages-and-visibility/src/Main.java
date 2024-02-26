7438af8842823e32640290c84c0eee500abbe754
Izabela França <ifranca.sc@gmail.com>
2024-02-26 19:41:58 -0300

    New Content

---
+++ constructors-packages-and-visibility/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Product product1 = new Product("Laptops", 25);
+
+    }
+}
---
+++ constructors-packages-and-visibility/src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+
+    String name;
+    int itemsInStock;
+
+    //Constructor
+    Product(String name, int itemsInStock) {
+        this.name = name;
+        this.itemsInStock = itemsInStock;
+    }
+}
